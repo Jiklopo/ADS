3b9c33f91a0f838615fe7cb47e1a7153f1121ba2
Timur Khaimovich <itima.com@gmail.com>
2019-09-12 11:41:04 +0600

    infinite queue with vector

--- lab1/infinite queue.cpp
+++ lab1/infinite queue.cpp
@@ -1,49 +1,11 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-struct InfQueue{
-    private:
-        vector<int> queue;
-        int first, last;
-    
-    public:
-        void clear(){
-            first = 0;
-            last = 0;
-            queue.clear();
-        }
-
-        void push(int value){
-            queue.push_back(value);
-            last++;
-        }
-
-        int pop(){
-            int value = queue.front();
-            queue.erase(queue.begin());
-            return value;
-        }
-
-        int front(){
-            return queue.front();
-        }
-
-        int getSize(){
-            return queue.size();
-        }
-};
+#include "infinite_queue.h"
 
 int main(){
     InfQueue q;
-    q.clear();
     string input;
     while (true){
         cin >> input;
-        bool isSizeZero = q.getSize() == 0;
         if(input == "exit"){
-            cout << "bye";
             break;
         }
         else if(input == "push"){
@@ -52,24 +14,25 @@ int main(){
             q.push(a);
         }
         else if(input == "pop"){
-            if(isSizeZero)
+            if(q.size() == 0)
                 cout << "Error" << endl;
             else 
                 cout << q.pop() << endl;
         }
         else if(input == "size"){
-            cout << q.getSize() << endl;
+            cout << q.size() << endl;
         }
         else if(input == "clear"){
-            q.clear();
+            q = InfQueue();
             cout << "ok" << endl;
         }
         else if(input == "front"){
-            if(isSizeZero)
+            if(q.size() == 0)
                 cout << "Error" << endl;
             else 
                 cout << q.front() << endl;
         }
     }
+    cout << "bye";
     return 0;
 }
